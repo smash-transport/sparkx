4b409de65c6fdca4d17ae7fb509e8d4f4185529d
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2023-04-13 16:19:51 +0200

    add checks for order of parameters in cut functions of Jetscape class

--- src/particle-analysis/OscarClass.py
+++ src/particle-analysis/OscarClass.py
@@ -644,10 +644,3 @@ class Oscar:
             raise TypeError('Input value must be a number or a tuple ' +\
                             'with the cut limits (cut_min, cut_max)')        
         return self
-
-
-  
-PATH = '/Users/nils/smash-devel/build/data/0/particle_lists.oscar'
-aaa = Oscar(PATH, events=(0,1))
-
-print(aaa.particle_list()[1][1])
