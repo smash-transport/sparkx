026f0337621615162d7b3fa860a5e241cd802c72
Nils Saß <nsass@fias.uni-frankfurt.de>
2023-06-15 17:55:07 +0200

    change Oscar documentation

--- src/particle-analysis/Oscar.py
+++ src/particle-analysis/Oscar.py
@@ -14,8 +14,9 @@ class Oscar:
     allow to directly act on all contained events as applying acceptance filters
     (e.g. un/charged particles, spectators/participants) to keep/romove particles
     by their PDG codes or to apply cuts (e.g. multiplicity, pseudo/rapidity, pT).
-    Once these filters are applied, the new data set can be saved 1) as a nested 
-    list containing all quantities of the Oscar format 2) as a list containing
+    Once these filters are applied, the new data set can be saved as 
+        1) a nested list containing all quantities of the Oscar format 
+        2) as a list containing
     Particle objects from the ParticleClass or it can be printed to a file 
     complying with the input format.
     
