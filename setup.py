f4f46eaa99672eeaaef4ba113e1ea435fd17bb5e
Niklas Götz <goetz@fias.uni-frankfurt.de>
2023-07-14 09:13:47 +0200

    Cleanup requirements, prepare PyPI setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sparkx',
+    packages=find_packages(where='src/sparkx'),
+    package_dir={'': 'sparkx'},
+    install_requires=[
+        "particle==0.22.0"
+        "numpy==1.23.5"
+        "scipy==1.10.1"
+        "matplotlib==3.7.1"
+        "abc-property==1.0"
+        "fastjet==3.4.1.2"
+    ],
+    version='1.0',
+    description='Software Package for Analyzing Relativistic Kinematics in Collision eXperiments',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author='Hendrik Roch, Nils Sass, Niklas Götz',
+    author_email="roch@fias.uni-frankfurt.de, sass@fias.uni-frankfurt.de, goetz@fias.uni-frankfurt.de",
+    url="https://hendrik1704.github.io/sparkx/",
+    download_url="https://github.com/Hendrik1704/sparkx",
+    license='MIT',
+    include_package_data = True
+)
