b93db2051940b03fe4d515e1888753c2ff61772f
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2023-10-18 09:25:41 +0200

    Add option to import with *

--- src/sparkx/flow/__init__.py
+++ src/sparkx/flow/__init__.py
@@ -1,1 +1,4 @@
- 
+from sparkx.flow.ReactionPlaneFlow import ReactionPlaneFlow
+from sparkx.flow.EventPlaneFlow import EventPlaneFlow
+from sparkx.flow.ScalarProductFlow import ScalarProductFlow
+from sparkx.flow.GenerateFlow import GenerateFlow
