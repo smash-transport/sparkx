84a7c6e2b58e4871c91c398706e486b8ca8e4c4b
Nils Saß <nsass@itp.uni-frankfurt.de>
2024-03-27 16:01:38 +0100

    Adjust Oscar tests

--- tests/test_Oscar.py
+++ tests/test_Oscar.py
@@ -1,12 +1,22 @@
 from sparkx.Oscar import Oscar
 import numpy as np
 import pytest
+import os
 
-OSCAR_FILE = "./test_files/particle_lists.oscar"
 OSCAR_EXTENDED_FILE = "./test_files/particle_lists_extended.oscar"
 
+@pytest.fixture
+def oscar_file_path():
+    # Assuming your test file is in the same directory as test_files/
+    return os.path.join(os.path.dirname(__file__), 'test_files', 'particle_lists.oscar')
 
-def test_constructor_invalid_initialization():
+@pytest.fixture
+def oscar_extended_file_path():
+    # Assuming your test file is in the same directory as test_files/
+    return os.path.join(os.path.dirname(__file__), 'test_files', 'particle_lists_extended.oscar')
+
+
+def test_constructor_invalid_initialization(oscar_file_path):
     # Initialization with invalid input file
     invalid_input_file = "./test_files/not_existing_file"
     with pytest.raises(FileNotFoundError):
@@ -14,22 +24,22 @@ def test_constructor_invalid_initialization():
         
     # Initalization with invalid kwargs: events not a number
     with pytest.raises(TypeError):
-        Oscar(OSCAR_FILE, events=np.nan)
+        Oscar(oscar_file_path, events=np.nan)
         
     with pytest.raises(TypeError):
-        Oscar(OSCAR_FILE, events=("a", "b"))
+        Oscar(oscar_file_path, events=("a", "b"))
         
     # Initalization with invalid kwargs: events negative
     with pytest.raises(ValueError):
-        Oscar(OSCAR_FILE, events=-1)
+        Oscar(oscar_file_path, events=-1)
         
     with pytest.raises(ValueError):
-        Oscar(OSCAR_FILE, events=(-4, -1))
+        Oscar(oscar_file_path, events=(-4, -1))
         
     # Initalization with invalid kwargs: events out of boundaries
     with pytest.raises(IndexError):
-        Oscar(OSCAR_FILE, events=5)
+        Oscar(oscar_file_path, events=5)
         
     with pytest.raises(IndexError):
-        Oscar(OSCAR_FILE, events=(5, 10))
+        Oscar(oscar_file_path, events=(5, 10))
         
