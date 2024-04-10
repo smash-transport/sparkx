38ae0b1b4d413aa8bd23f374567d0bf7c628c128
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2024-04-10 09:23:00 -0400

    Fix typos

--- src/sparkx/JetAnalysis.py
+++ src/sparkx/JetAnalysis.py
@@ -32,7 +32,7 @@ class JetAnalysis:
     jet_R_: float
         Jet radius parameter.
     jet_eta_range_: tuple
-        Mimimum and maximum pseudorapidity for jet selection.
+        Minimum and maximum pseudorapidity for jet selection.
     jet_pt_range_: tuple
         Minimum transverse momentum for jet selection and maximum transverse
         momentum to write out the jet.
@@ -411,7 +411,7 @@ class JetAnalysis:
                 # create a new file for the first event in the dataset
                 new_file = True
 
-            # perform the jet finiding algorithm
+            # perform the jet finding algorithm
             cluster = fj.ClusterSequence(event_PseudoJets, jet_definition)
             jets = fj.sorted_by_pt(cluster.inclusive_jets(self.jet_pt_range_[0]))
             jets = jet_selector(jets)
