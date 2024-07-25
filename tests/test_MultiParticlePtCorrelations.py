fb2a7ce43901f1884c41dd45dbdc25ba09d1bf32
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2024-07-25 08:43:48 -0400

    Add more physics tests

--- tests/test_MultiParticlePtCorrelations.py
+++ tests/test_MultiParticlePtCorrelations.py
@@ -9,6 +9,7 @@
 
 from sparkx.MultiParticlePtCorrelations import MultiParticlePtCorrelations
 from sparkx.Oscar import Oscar
+from sparkx.Particle import Particle
 import pytest
 import os
 import numpy as np
@@ -168,3 +169,45 @@ def test_MultiParticlePtCorrelations_mean_pt_correlations_and_cumulants(mpc_inst
             seed=seed)
     assert corr[0] == cumulants[0]
     assert corr_err[0] == cumulants_err[0]
+
+def test_MultiParticlePtCorrelations_mean_pt_correlations_and_cumulants_physics(mpc_instance):
+    # test with a dummy particle list, where all particles have the same pt
+    particle_list = []
+    for _ in range(10): # create 10 events
+        event_particles = []
+        for _ in range(100): # create 100 particles with the same pt
+            p_array = np.array([0,2114,11,2.01351754,1.30688601,-0.422958786,-0.512249773])
+            p = Particle(input_format="JETSCAPE", particle_array=p_array)
+            event_particles.append(p)
+        particle_list.append(event_particles)
+
+    compute_error=True
+    delete_fraction=0.2
+    number_samples=100
+    seed=42
+
+    corr, corr_err = mpc_instance.mean_pt_correlations(
+            particle_list_all_events=particle_list,
+            compute_error=compute_error,
+            delete_fraction=delete_fraction,
+            number_samples=number_samples,
+            seed=seed)
+    cumulants, cumulants_err = mpc_instance.mean_pt_cumulants(
+            particle_list_all_events=particle_list,
+            compute_error=compute_error,
+            delete_fraction=delete_fraction,
+            number_samples=number_samples,
+            seed=seed)
+    assert corr[0] == cumulants[0]
+    assert corr_err[0] == cumulants_err[0]
+
+    assert not np.isnan(corr).any()
+    assert not np.isnan(corr_err).any()
+
+    assert not np.isnan(cumulants).any()
+    assert not np.isnan(cumulants_err).any()
+
+    # if there is no variation in pt, the uncertainty should be zero
+    # check that the uncertainty is smaller 1e-10
+    assert np.all(corr_err < 1e-10)
+    assert np.all(cumulants_err < 1e-10)
