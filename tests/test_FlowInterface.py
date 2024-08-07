ba73173ad148653c34eb4fd6fe9e37c538398aed
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2024-08-07 11:42:39 -0400

    Format FlowInterface test

--- tests/test_FlowInterface.py
+++ tests/test_FlowInterface.py
@@ -9,6 +9,7 @@
 
 from sparkx.flow.FlowInterface import FlowInterface
 
+
 class DummyFlow(FlowInterface):
     def __init__(self, *args, **kwargs):
         self.args = args
@@ -17,16 +18,21 @@ class DummyFlow(FlowInterface):
     def integrated_flow(self, particle_data, *args, **kwargs):
         return "integrated_flow called"
 
-    def differential_flow(self, particle_data, bins, flow_as_function_of, *args, **kwargs):
+    def differential_flow(
+        self, particle_data, bins, flow_as_function_of, *args, **kwargs
+    ):
         return "differential_flow called"
-    
+
 
 def test_integrated_flow():
     flow = DummyFlow()
     result = flow.integrated_flow("particle_data")
     assert result == "integrated_flow called"
 
+
 def test_differential_flow():
     flow = DummyFlow()
-    result = flow.differential_flow("particle_data", "bins", "flow_as_function_of")
+    result = flow.differential_flow(
+        "particle_data", "bins", "flow_as_function_of"
+    )
     assert result == "differential_flow called"
