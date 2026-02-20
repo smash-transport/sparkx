63ccb76956d2a81da68a8facdcc72274c64b2c15
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2026-02-20 09:22:24 +0200

    Update copyright years

--- src/sparkx/Utilities.py
+++ src/sparkx/Utilities.py
@@ -1,6 +1,6 @@
 # ===================================================
 #
-#    Copyright (c) 2023-2024
+#    Copyright (c) 2023-2025
 #      SPARKX Team
 #
 #    GNU General Public License (GPLv3 or later)
